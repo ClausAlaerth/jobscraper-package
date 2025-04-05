c7a8c385c82376af9196d0b89d7f95fa97d3404c
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-04-05 02:06:49 -0300

    feat: implemented the .xlsx conversion to test module

--- get_job_list/teste.py
+++ get_job_list/teste.py
@@ -3,21 +3,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from conversion import list_conversion  # type: ignore
+# import time
+
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
 class JobScraping:
 
     def __init__(self, domain: str):
         self.domain = domain
-        self.lista: list[dict] = []
+        self.lista: list = []
         self.navigator = webdriver.Chrome()
         self.wait = WebDriverWait(self.navigator, 10)
 
     def acessar(self):
         self.navigator.get(self.domain)
-
-        # self.navigator.maximize_window()
 
         fic_list = self.wait.until(EC.presence_of_all_elements_located(
             (By.CSS_SELECTOR, "article > h2 > a")))
@@ -29,14 +32,9 @@ class JobScraping:
             name = fic_list[i].get_attribute("text")
             link = fic_list[i].get_attribute("href")
 
-            self.lista.append({"Nome": name, "Link": link})
+            self.lista.append([name, link])
 
-        print(len(fic_list))
-
-        for i in self.lista:
-            print(i["Nome"], i["Link"])
-
-        time.sleep(100)
+        list_conversion(self.lista, "arquivotestedois", "fanfic")
 
 
 if __name__ == "__main__":
