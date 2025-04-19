a50a185ae999874bbeddfca298c1918956cd169c
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-04-19 02:01:32 -0300

    refactor: updated to 0.0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="jb-scraper",
-    version="0.0.6",
+    version="0.0.7",
     author="Lucas Aquino",
     author_email="lc.aquinodeoliveira@gmail.com",
     description="JobScraper, um pacote de raspagem de vagas na internet.",
